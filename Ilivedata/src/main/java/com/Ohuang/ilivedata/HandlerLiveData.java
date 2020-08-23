a7a4abe6bef1e417bfc454d03bd3225cf80d2003
Ohuang <3077932030@qq.com>
2020-08-23 21:06:57 +0800

    Initial commit

---
+++ Ilivedata/src/main/java/com/Ohuang/ilivedata/HandlerLiveData.java
@@ -0,0 +1,58 @@
+package com.Ohuang.ilivedata;
+
+import android.os.Message;
+
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import java.util.HashMap;
+
+public class HandlerLiveData {
+    private static HandlerLiveData handlerLiveData = null;
+    private HashMap<String, MutableLiveData<Message>> map;
+
+    private HandlerLiveData() {
+        map = new HashMap<>();
+
+    }
+
+    public static HandlerLiveData getInstance() {
+        if (handlerLiveData == null) {
+            handlerLiveData = new HandlerLiveData();
+        }
+        return handlerLiveData;
+    }
+
+    private MutableLiveData<Message> with(String key) {
+        if (!map.containsKey(key)) {
+            map.put(key, new MutableLiveData<Message>());
+        }
+        return map.get(key);
+    }
+
+    public void sendMsg(String key, Message message) {
+        with(key).postValue(message);
+    }
+
+
+    private Message lsMessage = new Message();
+    public void sendEmptyMessage(String key, int i) {
+        lsMessage.what = i;
+        with(key).postValue(lsMessage);
+    }
+
+    public void addListener(String key, LifecycleOwner lifecycleOwner, final HandlerLiveDataListener handlerLiveDataListener) {
+        with(key).observe(lifecycleOwner, new Observer<Message>() {
+            @Override
+            public void onChanged(Message message) {
+                handlerLiveDataListener.HandlerMsg(message);
+            }
+        });
+    }
+
+
+    interface HandlerLiveDataListener {
+        void HandlerMsg(Message message);
+    }
+}
---
+++ Ilivedata/src/main/java/com/Ohuang/ilivedata/MyLiveData.java
@@ -0,0 +1,53 @@
+package com.Ohuang.ilivedata;
+
+import android.app.Activity;
+
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import java.util.HashMap;
+
+
+public class MyLiveData {
+
+    private HashMap<String, MutableLiveData<Object>> map;
+
+
+    private static MyLiveData myLiveData = null;
+
+    private MyLiveData() {
+        map = new HashMap<>();
+    }
+
+    public static MyLiveData getInstance() {
+        if (myLiveData == null) {
+            myLiveData = new MyLiveData();
+        }
+        return myLiveData;
+    }
+
+
+//    public MutableLiveData get(String key) {
+//        MutableLiveData<Object> mutableLiveData = map.get(key);
+//        if (mutableLiveData == null) {
+//            mutableLiveData = new MutableLiveData<>();
+//            map.put(key, mutableLiveData);
+//            mutableLiveData = map.get(key);
+//        }
+//        return mutableLiveData;
+//    }
+
+    public synchronized <T> MutableLiveData<T> with(String key, Class<T> t) {
+
+        if (!map.containsKey(key)) {
+            map.put(key, (MutableLiveData<Object>) new MutableLiveData<T>());
+        }
+
+        return (MutableLiveData<T>) map.get(key);
+    }
+
+
+
+
+}
